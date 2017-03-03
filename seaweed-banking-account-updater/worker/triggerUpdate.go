eb334fc2cd4e1a2491a2820496665858fc24f983
Christian Braun <chbr@adorsys.de>
2017-03-03 20:52:23 +0100

    Make update parallized

--- seaweed-banking-account-updater/worker/triggerUpdate.go
+++ seaweed-banking-account-updater/worker/triggerUpdate.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/ChristianNorbertBraun/seaweed-banking/seaweed-banking-account-updater/database"
+	"github.com/ChristianNorbertBraun/seaweed-banking/seaweed-banking-account-updater/model"
 )
 
 var ticker *time.Ticker
@@ -40,53 +41,57 @@ func runUpdate() {
 	}
 
 	for _, update := range updates {
-		accountInfo, err := database.GetLatestAccountInfo(update.BIC, update.IBAN)
-
-		log.Printf("Working on update for bic %s iban %s", update.BIC, update.IBAN)
-
-		if err != nil {
-			log.Println("Unable to get latest account info", err)
-
-			return
-		}
-
-		latestTransactionTime, _ := time.Parse(time.RFC3339Nano, accountInfo.LatestTransaction)
-		log.Println("Accountinfo time: ", accountInfo.LatestTransaction)
-		log.Println("Update last transaction: ", update.LastTransaction.Format(time.RFC3339Nano))
-		if latestTransactionTime.
-			After(update.LastTransaction) {
-			log.Printf("AccountInfo already up to date")
-			err := database.DeleteUpdate(update.BIC, update.IBAN)
-
-			if err != nil {
-				log.Println("Unable to delete update entry ", err)
-			}
-		}
-
-		log.Println("About to get all Transactions after")
-		transactionsToUpdate, err := database.GetAllTransactionsForAccountAfter(
-			update.BIC,
-			update.IBAN,
-			accountInfo.LatestTransaction)
-		log.Println("Got all Transactions: ", len(transactionsToUpdate))
-
-		if err != nil {
-			log.Println("Unable to fetch transactions after: ", accountInfo.LatestTransaction)
-			log.Println("Error was: ", err)
-			return
-		}
-
-		for _, transaction := range transactionsToUpdate {
-			added, newAccountInfo := accountInfo.AddTransaction(transaction)
-
-			if !added {
-				database.CreateAccountInfo(accountInfo)
-				accountInfo = newAccountInfo
-			}
-		}
-
-		database.CreateAccountInfo(accountInfo)
+		go runUpdateFor(update)
 	}
 
 	log.Println("Done with updating")
+}
+
+func runUpdateFor(update *model.Update) {
+	accountInfo, err := database.GetLatestAccountInfo(update.BIC, update.IBAN)
+
+	log.Printf("Working on update for bic %s iban %s", update.BIC, update.IBAN)
+
+	if err != nil {
+		log.Println("Unable to get latest account info", err)
+
+		return
+	}
+
+	latestTransactionTime, _ := time.Parse(time.RFC3339Nano, accountInfo.LatestTransaction)
+	log.Println("Accountinfo time: ", accountInfo.LatestTransaction)
+	log.Println("Update last transaction: ", update.LastTransaction.Format(time.RFC3339Nano))
+	if latestTransactionTime.
+		After(update.LastTransaction) {
+		log.Printf("AccountInfo already up to date")
+		err := database.DeleteUpdate(update.BIC, update.IBAN)
+
+		if err != nil {
+			log.Println("Unable to delete update entry ", err)
+		}
+	}
+
+	log.Println("About to get all Transactions after")
+	transactionsToUpdate, err := database.GetAllTransactionsForAccountAfter(
+		update.BIC,
+		update.IBAN,
+		accountInfo.LatestTransaction)
+	log.Println("Got all Transactions: ", len(transactionsToUpdate))
+
+	if err != nil {
+		log.Println("Unable to fetch transactions after: ", accountInfo.LatestTransaction)
+		log.Println("Error was: ", err)
+		return
+	}
+
+	for _, transaction := range transactionsToUpdate {
+		added, newAccountInfo := accountInfo.AddTransaction(transaction)
+
+		if !added {
+			database.CreateAccountInfo(accountInfo)
+			accountInfo = newAccountInfo
+		}
+	}
+
+	database.CreateAccountInfo(accountInfo)
 }
