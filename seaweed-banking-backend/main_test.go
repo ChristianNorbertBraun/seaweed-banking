c1efdd026a1e5947467450dd0a357deba2a59a5e
Christian Braun <chbr@adorsys.de>
2017-03-09 00:21:35 +0100

    Switch == to !=

--- seaweed-banking-backend/main_test.go
+++ seaweed-banking-backend/main_test.go
@@ -239,7 +239,7 @@ func VerifyTransactions(fakeAcc fakeAccount) error {
 
 	for _, createdTransaction := range fakeAcc.transactions {
 		for _, readTransaction := range readAccountInfo.Transactions {
-			if createdTransaction == *readTransaction {
+			if createdTransaction != *readTransaction {
 
 				return fmt.Errorf("VerifyTransactions: Transaction: bic: %v iban: %v not found",
 					createdTransaction.BIC,
