dc988985d19563f4721a0dabf45902ac07ccf6ce
Christian Braun <chbr@adorsys.de>
2017-03-17 22:14:37 +0100

    Delete book entries when update is deleted

--- seaweed-banking-account-updater/database/update.go
+++ seaweed-banking-account-updater/database/update.go
@@ -1,6 +1,9 @@
 package database
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/ChristianNorbertBraun/seaweed-banking/seaweed-banking-account-updater/config"
 	"github.com/ChristianNorbertBraun/seaweed-banking/seaweed-banking-account-updater/model"
 )
@@ -37,4 +40,11 @@ func DeleteUpdate(bic string, iban string) error {
 	return session.DB(config.Configuration.Db.DBName).
 		C(updateCollection).
 		RemoveId(bic + iban)
+}
+
+// DeleteTransactionsFromBook deletes all transactions from bic until and including the given transaction
+func DeleteTransactionsFromBook(update *model.Update) error {
+	latestFileName := update.LastTransaction.Format(time.RFC3339Nano)
+	path := fmt.Sprintf("%s/%s/%s", config.Configuration.Seaweed.BookFolder, update.BIC, update.IBAN)
+	return filer.DeleteFilesUntil(path, latestFileName)
 }
